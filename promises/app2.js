32ebc18af2c4dfb2198525ba4fc0b8d4ec445818
Kéren Olmedo <keren.olmedo09@gmail.com>
2022-04-27 23:23:10 -0300

    ...

--- promises/app2.js
+++ promises/app2.js
@@ -1,4 +1,4 @@
-const appendFile = require('fs/promises');
+const {appendFile} = require('fs/promises');
 
 appendFile('meu_arquivo2.txt', 'TreinaWeb').then((a) => {
     console.log('Arquivo foi criado/atualizado com sucesso');
